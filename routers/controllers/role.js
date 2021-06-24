017206518221c5409ee6316ef5b37a6c34dccac0
Khuldoonqataesheh <kqataesheh@gmail.com>
2021-06-24 06:04:04 +0300

    createNewAuthor

--- routers/controllers/role.js
+++ routers/controllers/role.js
@@ -1,16 +1,22 @@
 const db = require("./../../db/db");
 
 const createNewRole = (req, res) => {
-	const { role } = req.body;
-	const query = `INSERT INTO roles (role) VALUES (?)`;
-	const data = [role];
-	db.query(query, data, (err, result) => {
-		if (err) throw err;
-		console.log("RESULT: ", result);
-		res.json(result);
-	  });
+  const { role } = req.body;
+  const query = `INSERT INTO roles (role) VALUES (?)`;
+  const data = [role];
+  let id;
+  db.query(query, data, (err, result) => {
+    if (err) throw err;
+    id = result.insertId;
+    const query_2 = `SELECT * FROM roles WHERE role_id=?;`;
+    const data2 = [id];
+    db.query(query_2, data2, (err, result) => {
+      if (err) throw err;
+      res.status(201).json(result);
+    });
+  });
 };
 
 module.exports = {
-	createNewRole,
+  createNewRole,
 };
--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -1,28 +1,32 @@
-const usersModel = require('./../../db/models/users');
-
-const createNewAuthor = (req, res) => {
-	const { firstName, lastName, age, country, email, password, role } = req.body;
-
-	const user = new usersModel({
-		firstName,
-		lastName,
-		age,
-		country,
-		email,
-		password,
-		role,
-	});
-
-	user
-		.save()
-		.then((result) => {
-			res.status(201).json(result);
-		})
-		.catch((err) => {
-			res.send(err);
-		});
+const db = require("./../../db/db");
+const bcrypt = require("bcrypt");
+const createNewAuthor = async (req, res) => {
+  const { firstName, lastName, age, country, email, password, role_id } =
+    req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const query = `INSERT INTO users ( firstName, lastName, age, country, email, password, role_id) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  const data = [
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    hashedPassword,
+    role_id,
+  ];
+  let id;
+  db.query(query, data, (err, result) => {
+    if (err) throw err;
+    id = result.insertId;
+    const query_2 = `SELECT * FROM users WHERE id=?;`;
+    const data2 = [id];
+    db.query(query_2, data2, (err, result) => {
+      if (err) throw err;
+      res.status(201).json(result);
+    });
+  });
 };
 
 module.exports = {
-	createNewAuthor,
+  createNewAuthor,
 };
